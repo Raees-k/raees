6e901fa3e7422e9e5eb988a6978c6677d7d88a19
unknown <fawaz@resemblesystems.com>
2023-10-09 18:22:47 +0530

    update 04-10-20231

---
+++ build/precache-manifest.a587187252ec79a3233245b5cd52ff32.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "893691afb621d9e56ed19b2280e9bf75",
+    "url": "/sites/apps/PMO/index.html"
+  },
+  {
+    "revision": "08d80c9a39b5141d450d",
+    "url": "/sites/apps/PMO/static/css/2.c11832e3.chunk.css"
+  },
+  {
+    "revision": "1dfd7293cdd32a2f6e67",
+    "url": "/sites/apps/PMO/static/css/main.8a16c30a.chunk.css"
+  },
+  {
+    "revision": "08d80c9a39b5141d450d",
+    "url": "/sites/apps/PMO/static/js/2.32e4976f.chunk.js"
+  },
+  {
+    "revision": "54c112bbcbd3b1eb48ab9a251f0586e5",
+    "url": "/sites/apps/PMO/static/js/2.32e4976f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1dfd7293cdd32a2f6e67",
+    "url": "/sites/apps/PMO/static/js/main.ea697694.chunk.js"
+  },
+  {
+    "revision": "bfa391be4b71fd7bd07d",
+    "url": "/sites/apps/PMO/static/js/runtime-main.75797fcb.js"
+  },
+  {
+    "revision": "b498335f6d355d73c2813cdb88408f61",
+    "url": "/sites/apps/PMO/static/media/alrashid-logo.b498335f.png"
+  }
+]);
---
+++ buildlat0/precache-manifest.7fc1100de1ab7be1d98c0246303fd3bf.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0504115f97f82bfd27e3e4ce23f8727d",
+    "url": "/sites/apps/PMO/index.html"
+  },
+  {
+    "revision": "bb03054dcef72a9e2528",
+    "url": "/sites/apps/PMO/static/css/2.c11832e3.chunk.css"
+  },
+  {
+    "revision": "bbb4ca3622f12a6c6439",
+    "url": "/sites/apps/PMO/static/css/main.3604128c.chunk.css"
+  },
+  {
+    "revision": "bb03054dcef72a9e2528",
+    "url": "/sites/apps/PMO/static/js/2.701ca7a4.chunk.js"
+  },
+  {
+    "revision": "18a63a2c610a0b64e68acea4ac5710c3",
+    "url": "/sites/apps/PMO/static/js/2.701ca7a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bbb4ca3622f12a6c6439",
+    "url": "/sites/apps/PMO/static/js/main.42be699f.chunk.js"
+  },
+  {
+    "revision": "bfa391be4b71fd7bd07d",
+    "url": "/sites/apps/PMO/static/js/runtime-main.75797fcb.js"
+  },
+  {
+    "revision": "b498335f6d355d73c2813cdb88408f61",
+    "url": "/sites/apps/PMO/static/media/alrashid-logo.b498335f.png"
+  }
+]);
